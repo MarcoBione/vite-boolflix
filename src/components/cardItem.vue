4a2e365bb4806f5e42f063794608851c0028f093
MarcoBione <marcobione.work@gmail.com>
2023-04-20 17:40:50 +0200

    Add some comments

--- src/components/cardItem.vue
+++ src/components/cardItem.vue
@@ -5,13 +5,15 @@
             <img :src="store.imagePath + store.fileSize + image" alt="" class="img-fluid d-block">
         </div>
 
+        <!-- informazioni legate al film -->
         <div class="_myinfocard text-center d-flex flex-column align-items-center justify-content-around">
             <p class="m-0 fs-5 text-white">{{ title }}</p>
+            <!-- votazioni -->
             <div>
                 <p class="text-warning fw-semibold">Voti:</p>
                 <p class="text-warning fw-semibold">{{ votes }}</p>
             </div>
-
+            <!-- nazionalità del film -->
             <img :src="'/images/' + checkFlags + '.png'" :alt="original_language" class="w-25">
         </div>
     </div>
